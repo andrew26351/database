a8eb2870ab30bddb805cad00807fc2d41f425226
Andrew2635 <andrewleone2007@gmail.com>
2025-11-17 08:50:48 +0100

    inserita delete

--- src/insert.php
+++ src/insert.php
@@ -1,30 +1,48 @@
 <?php
+    $servername = 'db';
+    $username = 'myuser';
+    $password = 'mypassword';
+    $database = 'myapp_db';
 
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-        $servername = "db";
-        $username = "myuser";
-        $password = "mypassword";
-        $database = "myapp_db";
+
+    if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["name"]) && !isset($_POST["delete_id"])){
 
         $conn = new mysqli($servername, $username, $password, $database);
 
-        if($conn -> connect_error){
-            die("Connessione fallita: " . $conn_error);
+        if ($conn->connect_error) {
+            die("Connessione fallita: " . $conn->connect_error);
         }
 
-        $q = "INSERT INTO utenti (nome, email) VALUES ('" . $_POST['name'] . "', ' " . $_POST['email'] . "')";
 
-        if($conn->query($q)){
-            echo "Eseguita con successo";
-            $conn->close();
-        }else{
+        $q = "INSERT INTO utenti (nome, email) VALUES ('". $_POST["name"] . "', '". $_POST["email"]."')";
+        
+        if($conn -> query($q)){
+            echo "<h1>Utente inserito correttamente!</h1>";
+        } else {
             echo "ERRORE!";
         }
 
+        $conn->close();
     }
 
-?>
 
+    if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["delete_id"])) {
+
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        if ($conn->connect_error) {
+            die("Connessione fallita: " . $conn->connect_error);
+        }
+
+        $delete_id = $_POST["delete_id"];
+        $q = "DELETE FROM utenti WHERE id = $delete_id";
+
+        $conn->query($q);
+        $conn->close();
+
+        echo "<h3>Utente eliminato!</h3>";
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -34,40 +52,68 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
-        <label for="name">Nome e Cognome:</label>
-        <input type="text" name="name" required>
-        <br><br>
-        <label for="email">Email:</label>
-        <input type="email" name="email" required>
-        <br><br>
-        <input type="submit" value="Inserisci">
-    </form>
+
+    <!-- FORM INSERIMENTO -->
+    <div id="form">
+        <form action="" method="post">
+            <label>Inserisci nome e cognome</label>
+            <input type="text" name="name">
+
+            <label for="email">Inserisci email</label>
+            <input type="email" name="email">
+
+            <input type="submit" value="Inserisci">
+        </form>
+    </div>
+    
+    <!-- visualizza utenti -->
     <div id="users">
+
         <?php
-        
-            $servername = "db";
-            $username = "myuser";
-            $password = "mypassword";
-            $database = "myapp_db";
-            
             $conn = new mysqli($servername, $username, $password, $database);
 
-            if($conn -> connect_error){
+            if ($conn->connect_error) {
                 die("Connessione fallita: " . $conn->connect_error);
             }
 
-            $q= "SELECT * FROM utenti";
-
-            echo $q;
-
+            $q = "SELECT * FROM utenti";
             $results = $conn->query($q);
-
-            $row = $results->fetch_array(MYSQLI_ASSOC);
-            print_r($row);
-            
-
         ?>
+
+        <h2>Lista utenti</h2>
+
+        <table cellpadding="6">
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Azioni</th>
+            </tr>
+
+            <?php
+                if ($results && $results->num_rows > 0) {
+
+                    while ($row = $results->fetch_array(MYSQLI_ASSOC)) {
+
+                        echo "<tr>";
+                        echo "<td>". $row["nome"] . "</td>";
+                        echo "<td>". $row["email"] . "</td>";
+                        echo "<td>
+                                <form method='post' action=''>
+                                    <input type='hidden' name='delete_id' value='". $row["id"] ."'>
+                                    <input type='submit' value='Elimina'>
+                                </form>
+                              </td>";
+                        echo "</tr>";
+                    }
+
+                } else {
+                    echo "<tr><td colspan='3'>Nessun utente trovato</td></tr>";
+                }
+
+                $conn->close();
+            ?>
+        </table>
+
     </div>
 </body>
 </html>
